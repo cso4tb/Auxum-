280ed3363a4a1b881fea97e1bc232897e79a966b
cso4tb <cso4tb@virginia.edu>
2016-02-07 20:19:49 -0500

    Form for Sign up

---
+++ form_action.php
@@ -0,0 +1,7 @@
+<html>
+<body>
+
+<?php echo $_POST["name"]; ?><br> , thank you for registering with Auxum. Additonal information will be sent to <?php echo $_POST["email"]; ?><br>
+
+</body>
+</html>
