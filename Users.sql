fb9ee56095ac5589b3ffc1b5effa86f3be362c02
Brian Zanti <brianzanti@gmail.com>
2016-02-24 20:40:58 -0500

    adding sql migrations file

--- Users.sql
+++ Users.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.2
+-- version 4.5.1
 -- http://www.phpmyadmin.net
 --
--- Host: localhost
--- Generation Time: Feb 25, 2016 at 01:32 AM
--- Server version: 10.1.9-MariaDB
--- PHP Version: 5.6.15
+-- Host: 127.0.0.1
+-- Generation Time: Feb 25, 2016 at 02:40 AM
+-- Server version: 10.1.10-MariaDB
+-- PHP Version: 7.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -17,24 +17,37 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `Auxum`
+-- Database: `auxum`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `Users`
+-- Table structure for table `users`
 --
 
-CREATE TABLE `Users` (
-  `Name` varchar(50) NOT NULL,
-  `E-Mail` varchar(50) NOT NULL,
-  `Address` varchar(100) NOT NULL,
-  `City` varchar(50) NOT NULL,
-  `State` char(2) NOT NULL,
-  `Zip Code` char(5) NOT NULL,
-  `Join Date` date NOT NULL
+CREATE TABLE `users` (
+  `name` varchar(50) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `address` varchar(100) NOT NULL,
+  `city` varchar(50) NOT NULL,
+  `state` char(2) NOT NULL,
+  `zip` varchar(5) NOT NULL,
+  `joined` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`name`, `email`, `address`, `city`, `state`, `zip`, `joined`) VALUES
+('Brian Zanti', 'brianzanti@gmail.com', '839 Smartts Lane', 'Leesburg', 'VA', '20176', '0000-00-00'),
+('Brian Zanti', 'brianzanti@gmail.com', '839 Smartts Lane', 'Leesburg', 'VA', '20176', '1994-01-02'),
+('', 'brianzanti@gmail.com', '839 Smartts Lane', 'Leesburg', 'VA', '20176', '1994-01-02'),
+('Brian Zanti', 'brianzanti@gmail.com', '839 Smartts Lane', 'Leesburg', 'VA', '20176', '1994-01-02'),
+('Brian Zanti', 'brianzanti@gmail.com', '839 Smartts Lane', 'Leesburg', 'VA', '20176', '1994-01-02'),
+('Brian', 'brianzanti@gmail.com', '803 Smartts Lane Northeast', 'Leesburg', 'Vi', '20176', '2016-02-25'),
+('Jon Stewart', 'jon@thedailyshow.com', '1 new york ave', 'New York', 'NY', '99999', '2016-02-25');
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
