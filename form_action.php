ec5e84facf229b6653b8d670b1d12719c6219924
mollysall <mes3fn@virginia.edu>
2016-02-24 19:32:31 -0500

    php form

--- form_action.php
+++
@@ -1,7 +0,0 @@
-<html>
-<body>
-
-<?php echo $_POST["name"]; ?><br> , thank you for registering with Auxum. Additonal information will be sent to <?php echo $_POST["email"]; ?><br>
-
-</body>
-</html>
